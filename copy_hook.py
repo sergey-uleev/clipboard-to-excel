1f7b33bba3abefcc99f784c80be778d778492669
Sergey <mazi77@bk.ru>
2022-08-22 20:40:05 +0500

    Не используется

--- copy_hook.py
+++ copy_hook.py
@@ -3,8 +3,9 @@ from time import sleep # Стандартный модуль
 import pyperclip # Установить: pip install pyperclip. Библиотека для работы с буфером обмена
 from excel import Excel
 
-def copy_hook(): # Функция которая будет срабатывать при копировании
-    sleep(0.1) # Пауза в 100 миллисекунд, чтобы текст успел попасть в буфер обмена. Без этого будет выводиться предыдущая информация из буфера.
-    filename = 'my_book.xlsx'
-    excel = Excel(filename)
-    excel.write(pyperclip.paste()) # Вставляем текст из буфера обмена в функцию записи в Excel
+def copy_hook(window): # Функция которая будет срабатывать при копировании
+    # sleep(0.1) # Пауза в 100 миллисекунд, чтобы текст успел попасть в буфер обмена. Без этого будет выводиться предыдущая информация из буфера.
+    # filename = 'my_book.xlsx'
+    # excel = Excel(filename)
+    # excel.write(pyperclip.paste()) # Вставляем текст из буфера обмена в функцию записи в Excel
+    window.open()
