f8dfbbfcbf646d39c6e7f1df3a1b0a4ef5a6528a
Sergey <mazi77@bk.ru>
2022-09-12 21:39:21 +0500

    добавлено правило копирования для Vedomstvo

--- validation.py
+++ validation.py
@@ -4,6 +4,7 @@ re_pkpvd = r'(PKPVD\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 re_pkpvdmfc = r'(PKPVDMFC\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 re_ofsite = r'(OfSite\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 re_other = r'(Other\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
+re_vedomstvo = r'(Vedomstvo\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 
 def validate(str):
-    return re.search(re_pkpvd, str) or re.search(re_pkpvdmfc, str) or re.search(re_ofsite, str) or re.search(re_other, str)
+    return re.search(re_pkpvd, str) or re.search(re_pkpvdmfc, str) or re.search(re_ofsite, str) or re.search(re_other, str) or re.search(re_vedomstvo, str)
