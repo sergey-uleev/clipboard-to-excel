a85dd183c6b97d4365810d13730bb832e0c14c95
sergey-uleev <mazi77@bk.ru>
2023-01-31 20:35:53 +0600

    Изменение правил валидации

--- validation.py
+++ validation.py
@@ -8,4 +8,4 @@ re_vedomstvo = r'(Vedomstvo\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 re_bc = r'(BC\-\d{4,4}\-\d{2,2}\-\d{2,2}\-\d+)'
 
 def validate(str):
-    return re.search(re_pkpvd, str) or re.search(re_pkpvdmfc, str) or re.search(re_ofsite, str) or re.search(re_other, str) or re.search(re_vedomstvo, str) or re.search(re_bc, str)
+    return re.match(re_pkpvd, str) or re.match(re_pkpvdmfc, str) or re.match(re_ofsite, str) or re.match(re_other, str) or re.match(re_vedomstvo, str) or re.match(re_bc, str)
